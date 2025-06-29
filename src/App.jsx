1266452a87608c15da3f08a742493496bbce4250
Asad <alasadee2021@gmail.com>
2025-06-29 11:55:12 +0200

    finaltask,node_modules
    git commit -m finaltask,node_modules

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Haeder from'./component/Header.jsx';
+import Banner from './component/Banner.jsx';
+import './style/style.css';
+import TeamWork from './component/TeamWork.jsx';
+import RegisterForm from './component/RegisterForm.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Haeder />
+      <Banner /> 
+      <TeamWork />
+       <RegisterForm/>
+     
+    
+       
+    </div>
+  );
+}
+
+export default App;
